cf661af0d74eaf570c83ab7d83550761ad6880cc
Gaurav Shukla <shuklanikk@gmail.com>
2024-09-15 12:26:27 +0530

    Prepare for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,70 @@
+//plugins {
+//    alias(libs.plugins.android.library)
+//    alias(libs.plugins.kotlin.android)
+//    `maven-publish`
+//}
+//
+//android {
+//    namespace = "gaurav.iosdialog"
+//    compileSdk = 34
+//
+//    defaultConfig {
+//        minSdk = 24
+//        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+//        consumerProguardFiles("consumer-rules.pro")
+//    }
+//
+//    buildTypes {
+//        release {
+//            isMinifyEnabled = false
+//            proguardFiles(
+//                getDefaultProguardFile("proguard-android-optimize.txt"),
+//                "proguard-rules.pro"
+//            )
+//        }
+//    }
+//
+//    compileOptions {
+//        sourceCompatibility = JavaVersion.VERSION_1_8
+//        targetCompatibility = JavaVersion.VERSION_1_8
+//    }
+//
+//    kotlinOptions {
+//        jvmTarget = "1.8"
+//    }
+//}
+//
+//dependencies {
+//    implementation(libs.androidx.core.ktx)
+//    implementation(libs.androidx.appcompat)
+//    implementation(libs.material)
+//    implementation(libs.androidx.activity)
+//    implementation(libs.androidx.constraintlayout)
+//
+//    testImplementation(libs.junit)
+//    androidTestImplementation(libs.androidx.junit)
+//    androidTestImplementation(libs.androidx.espresso.core)
+//}
+//
+//// Add the maven-publish plugin
+//afterEvaluate {
+//    publishing {
+//        publications {
+//            create<MavenPublication>("release") {
+//                from(components["release"])
+//                groupId = "com.github.GauravNikk" // GitHub username as groupId
+//                artifactId = "iosdialog"          // Library's artifactId
+//                version = "1.0.0"                // Version of your library
+//
+//                // If needed, include sources and Javadoc
+//                artifact(tasks["bundleReleaseAar"])
+//            }
+//        }
+//    }
+//}
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
     `maven-publish`
 }
 
@@ -35,15 +99,15 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.4.0")
+    implementation("com.google.android.material:material:1.5.0")
+    implementation("androidx.activity:activity-ktx:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
 
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
 
 // Add the maven-publish plugin
@@ -62,3 +126,4 @@ afterEvaluate {
         }
     }
 }
+
